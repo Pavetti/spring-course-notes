df0b82c5c87c292b7db310efe0676576a0ee3b5f
Rigoletto <jacek15.jj@gmail.com>
2024-01-27 14:37:06 +0100

    security course start

---
+++ securityCourse/src/main/java/pl/pavetti/securityCourse/SecurityCourseApplication.java
@@ -0,0 +1,13 @@
+package pl.pavetti.securityCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCourseApplication.class, args);
+	}
+
+}
