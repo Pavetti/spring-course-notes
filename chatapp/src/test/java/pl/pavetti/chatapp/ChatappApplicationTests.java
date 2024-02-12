b71b3c41e076c264f8c69485ddeb4f68a9c280b1
Rigoletto <jacek15.jj@gmail.com>
2024-02-12 20:26:10 +0100

    chat app set up

---
+++ chatapp/src/test/java/pl/pavetti/chatapp/ChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pavetti.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
